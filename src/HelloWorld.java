863f2ee41cc418ed3d06ab587a14c2cdbf77ad1f
Waqas Azeem <waqas80@Waqass-MacBook-Pro.local>
2017-08-24 23:53:38 +0400

    Update files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,14 @@
 
-public class HelloWorld {
+public class HelloWorld implements IHelloWorld {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world. This is my first Git project");
+	}
 
+	@Override
+	public void InterfacePrint() {
+		System.out.println("Print statement from Interface method");
 	}
 
 }
---
+++ src/IHelloWorld.java
@@ -0,0 +1,4 @@
+
+public interface IHelloWorld {
+	void InterfacePrint();
+}
